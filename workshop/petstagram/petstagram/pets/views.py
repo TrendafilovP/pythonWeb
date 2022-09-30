3a47d06df134ac595b3ecd125c354cbc2c091a76
TrendafilovP <plamen.trendafilov@outlook.com>
2022-10-01 00:23:24 +0300

    Some bugs fixed

--- workshop/petstagram/petstagram/pets/views.py
+++ workshop/petstagram/petstagram/pets/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def pet_add(request):
+    return None
+
+
+def pet_delete(request):
+    return None
+
+def pet_details(request):
+    return None
+
+def pet_edit(request):
+    return None
--- workshop/petstagram/petstagram/photos/views.py
+++ workshop/petstagram/petstagram/photos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def photo_add(request):
+    return None
+
+def photo_details(request):
+    return None
+
+def photo_edit(request):
+    return None
