1ffd29c00b3414ad32a2841410e9a494cd15109d
TrendafilovP <plamen.trendafilov@outlook.com>
2022-10-01 23:21:00 +0300

    Some burgs were fixed.

--- workshop/petstagram/petstagram/settings.py
+++ workshop/petstagram/petstagram/settings.py
@@ -117,9 +117,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIR = [BASE_DIR / "static"]
+STATICFILES_DIR = [ BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
